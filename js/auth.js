a5456a46604b3c2a0f2cd6ccd3280aa43c6cebf8
Roni <ronisank21@hotmail.com>
2026-09-20 16:24:16 -0300

    feat[RF1]: salvamento na sessionStore

--- js/auth.js
+++ js/auth.js
@@ -1,5 +1,6 @@
 import { usuarios } from '../js/listagem-usuarios.js';
 
+let usuarioLogado;
 
 export function login(usuario, senha) {
     return new Promise((resolve, reject) => {
@@ -9,12 +10,16 @@ export function login(usuario, senha) {
             return item.email === usuario && item.senha === senha;
         });
         if (usuarioEncontrado) {
-
             resolve('Cadastro encontrado')
-
+            usuarioLogado = ({
+                nome: usuarioEncontrado.nome,
+                email: usuarioEncontrado.email
+            });
+            console.log(usuarioLogado);
+            sessionStorage.setItem('usuario', JSON.stringify(usuarioLogado))
         } else {
 
-            reject("Dados incorretos. Favor verificar e tentar novamente");
+            reject("Dados incorretos. Favor verificar e tentar novamente.");
         }
 
     });
